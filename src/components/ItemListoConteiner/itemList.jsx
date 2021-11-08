9d79fbfdf7dfb3bfe6b4da57c62a5eb017e09db1
lucasdeep10 <lucasdeep10@gmail.com>
2021-11-08 16:28:09 -0300

    itemList

---
+++ src/components/ItemListoConteiner/itemList.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const ItemList = ({products}) => {
+    return ( 
+        <>
+        {
+            products.map(prod => <div clasName="card w-25 mt-5" key={prod.id}>
+            <div className="card-header">
+            {`${prod.name} - ${prod.categoria}`}
+            </div>
+            <div clasName="card.header">
+            <img src={prod.foto} alr= ''/>
+            {prod.price}
+            </div>
+            <div clasName="card-footer">
+                <button claseName="btn btn-outline-primary btn-block">
+                    detalle del producto
+                </button>
+            </div>
+            </div>
+            )
+            }
+</>
+        )
+
+    }
